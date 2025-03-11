b0cfb4acf85ba99662df1d633c0ee2b8ed9a5bc5
kietp20 <89638907+kietp20@users.noreply.github.com>
2025-03-11 14:58:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Lỗi kết nối: " . $e->getMessage();
+    echo "Lỗi kết nối: " . $e->getMessage(aloha);
 }
 ?>
